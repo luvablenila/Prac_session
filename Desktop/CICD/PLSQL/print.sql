0077917914c7206d141ea8838af1dba009ac56ca
luvable_nila <elavarasipalanivelu@gmail.com>
2023-09-26 12:52:15 +0530

    first commit

--- Desktop/CICD/PLSQL/print.sql
+++ Desktop/CICD/PLSQL/print.sql
@@ -1,13 +1,17 @@
 --Pl/SQL code
 /* print 1..10*/
 
-Declare 
-x Number  := 0
-Begin
-For i in 1..10 loop
-x :=x+i
-end loop;
-end;
+DECLARE
+  x NUMBER := 0;
+BEGIN
+  FOR i IN 1..10 LOOP
+    x := x + i;
+  END LOOP;
+  
+  -- Display the result
+  DBMS_OUTPUT.PUT_LINE('The sum of integers from 1 to 10 is: ' || x);
+END;
+
 
 
 
